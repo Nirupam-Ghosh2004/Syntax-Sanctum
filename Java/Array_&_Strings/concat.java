c0e71472225867ab6b118176bf15dcdc7057b5f7
Nirupam-Ghosh2004 <tuhingh77@gmail.com>
2025-02-19 11:30:31 +0530

    Java Concat problem

---
+++ Java/Array_&_Strings/concat.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class concat
+{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        String s1 = in.nextLine();
+        String s2 = in.nextLine();
+        String s3 = s1+s2;
+        String str = "";
+        int len=s3.length();
+        for(int i=len-1;i>=0;i--)
+        {
+            str = str + s3.charAt(i);
+        }
+        System.out.println(str);
+    }
+}
