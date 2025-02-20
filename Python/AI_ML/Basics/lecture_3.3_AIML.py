f76733ef0eaac17c45bf31021a30469411fdb9f4
Nirupam-Ghosh2004 <tuhingh77@gmail.com>
2025-02-20 14:44:24 +0530

    Logistics Regression

--- Python/AI_ML/Basics/lecture_3.3_AIML.py
+++ Python/AI_ML/Basics/lecture_3.3_AIML.py
@@ -8,7 +8,7 @@ Created on Thu Jan 30 12:32:43 2025
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn import linear_model
+#from sklearn import linear_model
 
 path = "/home/tuhin/Documents/Syntax-Sanctum/Python/AI_ML/Basics/Data_Set/"
 data = pd.read_csv(path+"Iris_Dataset.csv")
