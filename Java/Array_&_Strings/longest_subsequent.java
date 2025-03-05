c1d94e653d6884160e26e0bb91fe79089446d5bd
Nirupam-Ghosh2004 <tuhingh77@gmail.com>
2025-03-05 20:09:22 +0530

    subsequent updated

--- Java/Array_&_Strings/longest_subsequent.java
+++ Java/Array_&_Strings/longest_subsequent.java
@@ -1,5 +1,5 @@
 import java.util.*;
-
+import java.util.Arrays; 
 class longest_subsequent {
     void path(int arr[], int l,int h)
     {
@@ -18,18 +18,18 @@ class longest_subsequent {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         longest_subsequent ob = new longest_subsequent();
-        System.out.println("Enter the size of the array : ");
+        System.out.print("Enter the size of the array : ");
         int n = in.nextInt();
         int arr[] = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = 0;
         }
         ob.print(arr, n);
-        System.out.println("Enter how many pair you want to enter : ");
+        System.out.print("Enter how many pair you want to enter : ");
         int v = in.nextInt();
         for(int i=0; i<v; i++)
         {
-            System.out.println("Enter the pair with a space : ");
+            System.out.println("Enter the pair  : ");
             int arr1[]= new int[2];
             arr1[0]=in.nextInt();
             arr1[1]=in.nextInt();
@@ -51,7 +51,16 @@ class longest_subsequent {
                 p++;
             }
         }
-        System.out.println("Number of sets whose elements are less than ");
+        arr2[p]=c;
+        int max = arr2[0];
+        for(int i =0; i<=p; i++)
+        {
+            if(arr2[i]>max)
+            {
+                max=arr2[i];
+            }
+        }
+        System.out.println("Longest path :  "+max);
         in.close();
     }
 }
