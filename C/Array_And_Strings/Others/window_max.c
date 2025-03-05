2ff7df1db7e2b1b55e40f5615bea7f0a4911ecb3
Nirupam-Ghosh2004 <tuhingh77@gmail.com>
2025-03-05 12:21:15 +0530

    Window maximum problem

---
+++ C/Array_And_Strings/Others/window_max.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+void maximum(int *arr, int l, int h)
+{
+    int m = arr[l];
+    for(int i=l; i<=h; i++)
+    {
+        if(arr[i]>m)
+        {
+            m = arr[i];
+        }
+    }
+    printf("%d ",m);
+}
+void print(int *arr, int n)
+{
+    printf("The array elements are : ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+int main()
+{
+    int n;
+    printf("Enter the size of the array : ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter elements in the array : \n");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    print(arr, n);
+    int x;
+    printf("Enter the window size : ");
+    scanf("%d", &x);
+    for(int i =0 ; i<=n-x; i++)
+    {
+        maximum(arr,i,i+x-1);
+    }
+}
