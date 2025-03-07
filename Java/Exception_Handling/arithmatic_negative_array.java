dbbf92f01b6acd2f59128206e49c7b2a8da17c3c
Nirupam-Ghosh2004 <tuhingh77@gmail.com>
2025-03-07 15:53:28 +0530

    updated

---
+++ Java/Exception_Handling/arithmatic_negative_array.java
@@ -0,0 +1,32 @@
+package Java.Exception_Handling;
+
+import java.util.Scanner;
+
+public class arithmatic_negative_array {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a : ");
+        int a = in.nextInt();
+        System.out.println("Enter b : ");
+        int b = in.nextInt();
+        int c;
+        try{
+            c=a/b;
+            System.out.println("Output : "+c);
+        }
+        catch(Exception p){
+            System.out.println(p);
+        }
+        int arr[]={1,2,3,4,5};
+        arr[-1]=10;
+        // System.out.println("Enter the index : ");
+        // int x = in.nextInt();
+        // in.close();
+        // if(x<0)
+        // {
+        //     throw new ArrayIndexOutOfBoundsException("Negative index : "+x);
+        // }
+        // System.out.println(arr[x]);
+        in.close();
+    }
+}
