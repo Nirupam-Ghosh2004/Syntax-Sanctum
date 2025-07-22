686e04a0dc36d14ead0660bcb5c0eeaf629f00a2
Nirupam-Ghosh2004 <tuhingh77@gmail.com>
2025-07-23 01:02:43 +0530

    updated

--- C++/Data_Structures/Stack/del_mid.cpp
+++ C++/Data_Structures/Stack/del_mid.cpp
@@ -4,17 +4,18 @@ using namespace std;
 int main()
 {
     stack<int> st;
+
     int len;
     cout<<"Enter size of the stack : ";
     cin>>len;
-    cout<<"Enter elements into the stack : ";
+    cout<<"Enter elements into the stack : "<<endl;
     for(int i = 0; i<len; i++)
     {
         int ele;
         cin>>ele;
         st.push(ele);
     }
-    int h = (len%2==0)?(len/2)-1:(len/2);
-
+    int m = (len%2==0)?(len/2)-1:(len/2);
+    cout<<"Middle index : "<<m<<endl;
     return 0;
 }
